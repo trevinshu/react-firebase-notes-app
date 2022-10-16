691340b914bbb6f39bdbd517bd47024609423a55
Trevin Shu <trevinshu2008@gmail.com>
2022-10-15 21:42:15 -0600

    added user dependency to useEffect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ function Login() {
     if (user) {
       navigate('/');
     }
-  }, []);
+  }, [user]);
 
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
